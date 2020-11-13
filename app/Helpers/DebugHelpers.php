e6056362d1f8d28d28a72b031887aa61acc651fc
Donovan Chandler <d_chandler@apple.com>
2020-11-13 10:20:35 -0800

    Report memory usage in KB

--- app/Helpers/DebugHelpers.php
+++ app/Helpers/DebugHelpers.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Log;
 
 class DebugHelpers
 {
-    public static function logMemoryHeader()
+    public static function logMemoryHeader(): void
     {
         static::logMemory('row', ['current KB', 'peak KB']);
     }
 
-    public static function logMemory(?string $message = null, ?array $context = null)
+    public static function logMemory(?string $message = null, ?array $context = null): void
     {
-        $context = !is_null($context) ? $context : [memory_get_usage(), memory_get_peak_usage()];
+        $context = !is_null($context) ? $context : [memory_get_usage()/1024, memory_get_peak_usage()/1024];
         Log::channel('memory')->debug($message, $context);
     }
 }
